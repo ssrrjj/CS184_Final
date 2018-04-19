c76a0ceb7d245a043c2946229106716e19cb11f6
ssrrjj <shaorj@berkeley.edu>
2018-04-18 21:47:36 -0700

    LFImageBuffer

--- src/image.h
+++ src/image.h
@@ -204,7 +204,165 @@ struct HDRImageBuffer {
 
 }; // class HDRImageBuffer
 
+struct LFImageBuffer {
+
+  /**
+   * Default constructor.
+   * The default constructor creates a zero-sized image.
+   */
+  LFImageBuffer() : w(0), h(0), subh(0), subw(0) { }
+
+  /**
+   * Parameterized constructor.
+   * Create an image of given size.
+   * \param w width of the image
+   * \param h height of the image
+   */
+  LFImageBuffer(size_t w, size_t h, size_t subw, size_t subh) : w(w), h(h), subh(subh), subw(subw) { 
+    
+    data.resize(w * h);
+    for (auto &grid : data) {
+      grid.resize(subh * subw);
+    }
+    
+  }
+
+  /**
+   * Resize the image buffer.
+   * \param w new width of the image
+   * \param h new height of the image
+   */
+  void resize(size_t w, size_t h) {
+    this->w = w;
+    this->h = h;
+    data.resize(w * h);
+    clear();
+  }
+
+  /**
+   * Update the color of a given pixel.
+   * \param s new spectrum value to be set
+   * \param x row of the pixel
+   * \param y column of the pixel
+   */
+  void update_pixel(std::vector<Spectrum>& grid, size_t x, size_t y) {
+    // assert(0 <= x && x < w);
+    // assert(0 <= y && y < h);
+    data[x + y * w] = grid;
+  }
+
+  /**
+   * Update the color of a given pixel. Blend new pixel color with current
+   * pixel data in the buffer according to the blending factor. The result
+   * of this would be buffer[i] = s * r + buffer[i] * (1-r);
+   * \param s new spectrum value to be set
+   * \param x row of the pixel
+   * \param y column of the pixel
+   * \param r blending factor
+   */
+
+  /*
+  void update_pixel(const Spectrum& s, size_t x, size_t y, float r) {
+    // assert(0 <= x && x < w);
+    // assert(0 <= y && y < h);
+    data[x + y * w] = s * r + (1 - r) * data[x + y * w];
+  }
+*/
+
+  /**
+   * Tonemap and convert to color space image.
+   * \param target target color buffer to store output
+   * \param gamma gamma value
+   * \param level exposure level adjustment
+   * \key   key value to map average tone to (higher means brighter)
+   * \why   white point (higher means larger dynamic range)
+   */
+  /*
+  void tonemap(ImageBuffer& target,
+    float gamma, float level, float key, float wht) {
+
+    // compute global log average luminance!
+    float avg = 0;
+    for (size_t i = 0; i < w * h; ++i) {
+      // the small delta value below is used to avoids singularity
+      avg += log(0.0000001f + data[i].illum());
+    }
+    avg = exp(avg / (w * h));
+
+
+    // apply on pixels
+    float one_over_gamma = 1.0f / gamma;
+    float exposure = sqrt(pow(2,level));
+    for (size_t y = 0; y < h; ++y) {
+      for (size_t x = 0; x < w; ++x) {
+        Spectrum s = data[x + y * w];
+        float l = s.illum();
+        s *= key / avg;
+        s *= ((l + 1) / (wht * wht)) / (l + 1);
+        float r = pow(s.r * exposure, one_over_gamma);
+        float g = pow(s.g * exposure, one_over_gamma);
+        float b = pow(s.b * exposure, one_over_gamma);
+        target.update_pixel(Color(r, g, b, 1.0), x, y);
+      }
+    }
+  }
+*/
+  /**
+   * Convert the given tile of the buffer to color.
+   */
+  void toColor(ImageBuffer& target, size_t x0, size_t y0, size_t x1, size_t y1) {
+
+    float gamma = 2.2f;
+    float level = 1.0f;
+    float one_over_gamma = 1.0f / gamma;
+    float exposure = sqrt(pow(2,level));
+    for (size_t y = y0; y < y1; ++y) {
+      for (size_t x = x0; x < x1; ++x) {
+        std::vector<Spectrum> grid = data[x + y * w];
+        Spectrum s = Spectrum(0, 0, 0);
+        int i, j;
+        for (auto &spec : grid) {
+          s += spec;
+        }
+        s = s/float(subw * subh);
+//        const Spectrum& s = data[x + y * w];
+        float r = pow(s.r * exposure, one_over_gamma);
+        float g = pow(s.g * exposure, one_over_gamma);
+        float b = pow(s.b * exposure, one_over_gamma);
+        target.update_pixel(Color(r, g, b, 1.0), x, y);
+      }
+    }
+  }
+
+  /**
+   * If the buffer is empty
+   */
+  bool is_empty() { return (w == 0 && h == 0); }
+
+  /**
+   * Clear image buffer.
+   */
+  void clear() { 
+    for (auto &grid: data) {
+
+      memset(&grid[0], 0, subw*subh*sizeof(Spectrum));
+
+    }
+  }
+
+  size_t w; ///< width
+  size_t h; ///< height
+  size_t subw;
+  size_t subh;
+
+  std::vector<std::vector<Spectrum>> data; ///< pixel buffer
+
+}; // class HDRImageBuffer
 
 } // namespace CGL
+
+
+
+
 
 #endif // CGL_IMAGE_H
