055a7acbf8fa16e57c7b45b5fb6ef1d9407b44fc
cs184 <157230542@qq.com>
2018-05-04 13:56:28 -0700

    add pictures

--- build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
+++ build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -1,38 +1,39 @@
 # The set of languages for which implicit dependencies are needed:
-set(CMAKE_DEPENDS_LANGUAGES
+SET(CMAKE_DEPENDS_LANGUAGES
   "C"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "/Users/wei/Study/Graphics/project/CS184_Final/CGL/deps/glfw/src/cocoa_init.m" "/Users/wei/Study/Graphics/project/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/cocoa_init.m.o"
-  "/Users/wei/Study/Graphics/project/CS184_Final/CGL/deps/glfw/src/cocoa_monitor.m" "/Users/wei/Study/Graphics/project/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o"
-  "/Users/wei/Study/Graphics/project/CS184_Final/CGL/deps/glfw/src/cocoa_window.m" "/Users/wei/Study/Graphics/project/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/cocoa_window.m.o"
-  "/Users/wei/Study/Graphics/project/CS184_Final/CGL/deps/glfw/src/context.c" "/Users/wei/Study/Graphics/project/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/context.c.o"
-  "/Users/wei/Study/Graphics/project/CS184_Final/CGL/deps/glfw/src/init.c" "/Users/wei/Study/Graphics/project/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/init.c.o"
-  "/Users/wei/Study/Graphics/project/CS184_Final/CGL/deps/glfw/src/input.c" "/Users/wei/Study/Graphics/project/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/input.c.o"
-  "/Users/wei/Study/Graphics/project/CS184_Final/CGL/deps/glfw/src/iokit_joystick.m" "/Users/wei/Study/Graphics/project/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/iokit_joystick.m.o"
-  "/Users/wei/Study/Graphics/project/CS184_Final/CGL/deps/glfw/src/mach_time.c" "/Users/wei/Study/Graphics/project/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/mach_time.c.o"
-  "/Users/wei/Study/Graphics/project/CS184_Final/CGL/deps/glfw/src/monitor.c" "/Users/wei/Study/Graphics/project/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/monitor.c.o"
-  "/Users/wei/Study/Graphics/project/CS184_Final/CGL/deps/glfw/src/nsgl_context.m" "/Users/wei/Study/Graphics/project/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/nsgl_context.m.o"
-  "/Users/wei/Study/Graphics/project/CS184_Final/CGL/deps/glfw/src/posix_tls.c" "/Users/wei/Study/Graphics/project/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/posix_tls.c.o"
-  "/Users/wei/Study/Graphics/project/CS184_Final/CGL/deps/glfw/src/window.c" "/Users/wei/Study/Graphics/project/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/window.c.o"
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/cs184/CS184_Final/CGL/deps/glfw/src/context.c" "/home/cs184/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/context.c.o"
+  "/home/cs184/CS184_Final/CGL/deps/glfw/src/glx_context.c" "/home/cs184/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/glx_context.c.o"
+  "/home/cs184/CS184_Final/CGL/deps/glfw/src/init.c" "/home/cs184/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/init.c.o"
+  "/home/cs184/CS184_Final/CGL/deps/glfw/src/input.c" "/home/cs184/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/input.c.o"
+  "/home/cs184/CS184_Final/CGL/deps/glfw/src/linux_joystick.c" "/home/cs184/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/linux_joystick.c.o"
+  "/home/cs184/CS184_Final/CGL/deps/glfw/src/monitor.c" "/home/cs184/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/home/cs184/CS184_Final/CGL/deps/glfw/src/posix_time.c" "/home/cs184/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/posix_time.c.o"
+  "/home/cs184/CS184_Final/CGL/deps/glfw/src/posix_tls.c" "/home/cs184/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/posix_tls.c.o"
+  "/home/cs184/CS184_Final/CGL/deps/glfw/src/window.c" "/home/cs184/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/window.c.o"
+  "/home/cs184/CS184_Final/CGL/deps/glfw/src/x11_init.c" "/home/cs184/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/x11_init.c.o"
+  "/home/cs184/CS184_Final/CGL/deps/glfw/src/x11_monitor.c" "/home/cs184/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/x11_monitor.c.o"
+  "/home/cs184/CS184_Final/CGL/deps/glfw/src/x11_window.c" "/home/cs184/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/x11_window.c.o"
+  "/home/cs184/CS184_Final/CGL/deps/glfw/src/xkb_unicode.c" "/home/cs184/CS184_Final/build/CGL/deps/glfw/src/CMakeFiles/glfw.dir/xkb_unicode.c.o"
   )
-set(CMAKE_C_COMPILER_ID "Clang")
+SET(CMAKE_C_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_C
+SET(CMAKE_TARGET_DEFINITIONS
   "_GLFW_USE_CONFIG_H"
   )
 
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
 # The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
+SET(CMAKE_C_TARGET_INCLUDE_PATH
   "../CGL/deps/glfw/src"
   "CGL/deps/glfw/src"
   )
-
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
-  )
-
-# Fortran module output directory.
-set(CMAKE_Fortran_TARGET_MODULE_DIR "")
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
